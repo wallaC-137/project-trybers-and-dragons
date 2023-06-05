e8506578a767afabc1651cda28237afb243642e8
wallaC-137 <wallace_031@hotmail.com>
2023-06-05 15:47:11 -0300

    req01: ok - cria classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,27 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+  private static _createdRacesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+    
+    Race._createdRacesInstances += 1;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+    return Race._createdRacesInstances;
+  }
+
+  abstract get maxLifePoints() : number;
+}
